38334ede85ff9d243ec46c58eae4de68e8f540e3
Oleg <olberd@bk.ru>
2025-09-15 14:21:59 +0300

    add dots

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import checkInputs from "./modules/checkInputs";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer('11 september 2025');
 menu();
 modal();
 checkInputs();
 tabs();
+slider();
