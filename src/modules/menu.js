aa420095f8493f07d9215e4e9036aeb4f312e055
Oleg <olberd@bk.ru>
2025-09-15 10:19:41 +0300

    add two handlers

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,25 +2,25 @@ const menu = () => {
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
     const closeBtn = menu.querySelector('.close-btn');
-    const menuItems = menu.querySelectorAll('ul>li>a')
+    const menuItems = menu.querySelectorAll('ul>li>a');
 
-    const handleMenu = () => {
-        menu.classList.toggle('active-menu');
-    }
-
-    menuBtn.addEventListener('click', handleMenu);
-    //
-    // closeBtn.addEventListener('click', handleMenu);
-    //
-    // menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
-
-    menu.addEventListener('click', (e) => {
-        if (e.target.classList.contains('close-btn') || e.target.matches('a')) {
-            handleMenu();
+    const toggleMenu = () => {
+        const handleMenu = () => {
+            menu.classList.toggle('active-menu');
         }
-    })
 
+        menuBtn.addEventListener('click', handleMenu);
 
+        menu.addEventListener('click', (e) => {
+            if (e.target.classList.contains('close-btn') ||
+                e.target.matches('a')) {
+
+                handleMenu();
+            }
+        });
+    };
+
+    toggleMenu();
 }
 
 export default menu
