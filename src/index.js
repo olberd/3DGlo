d9f46b4178b611de4c516b7ac3b509214aaa2dbf
Oleg <olberd@bk.ru>
2025-09-26 10:51:36 +0300

    add calculator

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from "./modules/modal";
 import checkInputs from "./modules/checkInputs";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
+import calc from "./modules/calc";
 
 timer('11 september 2025');
 menu();
@@ -11,3 +12,4 @@ modal();
 checkInputs();
 tabs();
 slider();
+calc();
